0e2432052fc6019070cf5b22d6d3e190745272d5
Joseph <joseph.hatzilazaridis@gmail.com>
2021-03-24 21:32:02 -0400

    connected to API

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Forecast(props) {
+    
+    let [responseObject, setResponseObject] = useState({});
+
+    function getForecast() {
+        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "5f419a5da2msha271fbdf14631e6p1244f8jsn7e7b2101ec38",
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            setResponseObject(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+
+    return (
+        <div>
+            <h2>Find Current Weather Conditions</h2>
+            <div>
+                {JSON.stringify(responseObject)}
+            </div>
+            <button onClick={getForecast}>Get Forecast</button>
+        </div>
+    )
+
+}
