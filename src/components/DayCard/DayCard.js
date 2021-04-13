397ac6cb0ad914467227b13acb23183d03a4e1be
Joseph <joseph.hatzilazaridis@gmail.com>
2021-04-13 18:19:29 -0400

    pushing app to heroku

--- src/components/DayCard/DayCard.js
+++ src/components/DayCard/DayCard.js
@@ -31,7 +31,7 @@ import React from 'react';
 import {Card} from 'react-bootstrap';
 
 const DayCard = ({dt, temp_min, temp_max, main, icon}) => {
-  // create a date object with Date class constructor
+
   const date = new Date(dt);
   
   return (
@@ -39,13 +39,11 @@ const DayCard = ({dt, temp_min, temp_max, main, icon}) => {
       
       <Card.Img
         variant="top"
-        // get the src from example url and pass the icon prop for icon code
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
       />
       
       <Card.Body>
         <Card.Title>{main}</Card.Title>
-        {/*  datetime is received in milliseconds */}
         <p>
           {date.toLocaleDateString()} - {date.toLocaleTimeString()}
         </p>
